c07c7ba06dc3fb41aa939d4778ecef7816b36100
Gusev A. Leonid <gla@cit-nnov.local>
2019-09-30 14:46:35 +0300

    Добавьте файлы проекта.

---
+++ Murunov.svod/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Murunov.svod.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
